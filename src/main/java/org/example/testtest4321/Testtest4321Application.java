a9331b1cc376e2ee34862ec9927bdc628c638a80
600gramSik <geunsikevin@naver.com>
2024-03-30 17:02:07 +0900

    플젝생성

---
+++ src/main/java/org/example/testtest4321/Testtest4321Application.java
@@ -0,0 +1,13 @@
+package org.example.testtest4321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testtest4321Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testtest4321Application.class, args);
+    }
+
+}
